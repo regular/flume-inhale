0282da39143149c1d94b565c82c80b491b706abf
Jan Bölsche <regular.gonzales@googlemail.com>
2023-11-16 09:37:13 +0100

    Treat continuation value as opaque, except for timestamp

--- index.js
+++ index.js
@@ -26,18 +26,25 @@ module.exports = function(conf, db, stream, cb) {
   conf = Object.assign({}, defaults, conf)
   //if (conf.data_dir) return cb(new Error('Need data_dir'))
 
-  db.use('continuation', Reduce(1, (acc, item) => {
-    if (item.type !== '__since') return acc
-    return Math.max(acc || 0, item.data.timestamp)
+  db.use('continuation', Reduce(2, {
+    reduce: (acc, item) => {
+      if (item.type !== '__since') return acc
+      return item.data.timestamp >= acc.timestamp ? item.data : acc
+    },
+    initial: {timestamp: 0}
   }))
    
-  db.continuation.get((err, value) => {
+  db.continuation.get((err, continuation) => {
     if (err) {
       console.error('Unable to read continuation value:', err.message)
       return cb(err)
     }
-    console.error('continuation value is', formatTimestamp(value))
-    conf.continuation = value
+    if (continuation.timestamp) {
+      console.error('continuation timestamp is', formatTimestamp(continuation.timestamp))
+    } else {
+      console.error('Starting from scratch')
+    }
+    conf.continuation = continuation
     
     function periodic() {
       update(db, conf, (err, continuation)=>{
@@ -47,7 +54,7 @@ module.exports = function(conf, db, stream, cb) {
           console.error(inspect(err, {depth: 6, colors: true}))
           return cb(err)
         } else {
-          console.error('flumedb is in sync with upstream source.')
+          //console.error('flumedb is in sync with upstream source.')
         }
         const ms = DateTime.fromSeconds(0).plus(conf.update_interval).toMillis()
         setTimeout(periodic, ms)
@@ -61,15 +68,15 @@ module.exports = function(conf, db, stream, cb) {
     let continuation = conf.continuation
 
     const dt_max = DateTime.now().setZone(conf.tz).minus(conf.minage)
-    const dt_start = conf.continuation ?
-      DateTime.fromSeconds(conf.continuation / 1000).setZone(conf.tz) :    
+    const dt_start = conf.continuation.timestamp ?
+      DateTime.fromSeconds(conf.continuation.timestamp / 1000).setZone(conf.tz) :    
       DateTime.fromISO(conf.startday).setZone(conf.tz).startOf('day')
 
     const dt_end = DateTime.min(dt_max, dt_start.plus(conf.maxSpanPerReq))
 
     const wait = dt_start.plus(conf.minSpanPerReq).toSeconds() - dt_end.toSeconds()
     if (wait>0) {
-      console.error(`It is too early, will retry ${DateTime.now().plus({seconds: wait}).toRelative()}`)
+      console.error(`Will continue ${DateTime.now().plus({seconds: wait}).toRelative()}`)
       setTimeout( ()=>{
         cb(null, continuation)
       }, wait * 1000)
@@ -88,7 +95,7 @@ module.exports = function(conf, db, stream, cb) {
           if (err) return cb(err)
           db.continuation.get((err, value) => {
             if (err) return cb(err)
-            console.error('new continuation value:', formatTimestamp(value))
+            console.error(formatTimestamp(value.timestamp), items.length, 'items')
             continuation = value
             cb(null, seq)
           })
